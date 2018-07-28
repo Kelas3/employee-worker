44fb0a0f40a2dd7defb060eaec792febdc697628
Kelas3 <37308591+Kelas3@users.noreply.github.com>
2018-07-28 14:47:54 -0500

    Update SalariedWorker.java

--- SalariedWorker.java
+++ SalariedWorker.java
@@ -1,13 +1,11 @@
 
 /**
- * Write a description of class SalariedWorker here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Simulates 
+ * @author Salek Khan
  */
 public class SalariedWorker
 {
-    // instance variables - replace the example below with your own
+    // instance variables 
     private int x;
 
     /**
@@ -18,16 +16,8 @@ public class SalariedWorker
         // initialise instance variables
         x = 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
     public int sampleMethod(int y)
-    {
-        // put your code here
+
         return x + y;
     }
 }
